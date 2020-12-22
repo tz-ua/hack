9dbbcb4b7341150aaeff514d3bb3b66068e334f8
Artur <artur.yermolenko@heroteams.com.au>
2020-12-22 10:51:23 +0200

    Add Workplace entity

---
+++ database/factories/WorkplaceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Workplace;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WorkplaceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Workplace::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
