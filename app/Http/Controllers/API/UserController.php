adadaf62671124bb8b9ea568388559d7d3f0886a
Dmytro Fedorchenko <Dmytro.Fedorchenko@elmopayroll.com.au>
2020-12-22 13:30:41 +0200

    Splitted user annotation to request and response

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
      *     @OA\Response(
      *         response=200,
      *         description="Successfull operation",
-     *         @OA\JsonContent(ref="#/components/schemas/UserSchema")
+     *         @OA\JsonContent(ref="#/components/schemas/UserSchemaRequest")
      *     )
      * )
      */
@@ -47,12 +47,12 @@ class UserController extends Controller
      *      @OA\RequestBody(
      *         required=true,
      *         description="Json Content",
-     *         @OA\JsonContent(ref="#/components/schemas/UserSchema")
+     *         @OA\JsonContent(ref="#/components/schemas/UserSchemaRequest")
      *     ),
      *     @OA\Response(
      *         response=200,
      *         description="Successfull operation",
-     *         @OA\JsonContent(ref="#/components/schemas/UserSchema")
+     *         @OA\JsonContent(ref="#/components/schemas/UserSchemaResponse")
      *     ),
      *      @OA\Response(
      *          response=422,
